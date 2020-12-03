047e49c1f51b3791e6077f01fc14fb4f244affa9
rudietuesdays <rae@throneless.tech>
2020-12-03 11:32:07 -0500

    building injected script and styles for integration with super

---
+++ src/super.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', (event) => {
+  const parent = document.getElementById('notion-app');
+  const footer = document.createElement("FOOTER");
+  const footerInner = document.createElement("DIV");
+  footerInner.innerHTML = "<a href='https://recoding.tech/Democracy-in-danger-ce1616459a9d4aedae6997e5b365c106'>Democracy in Danger</a> | <a href='https://recoding.tech/About-7755d738441b41d3bf29dc2a679e0a9a'>About</a> | <a href='https://recoding.tech/Newsletter-6811176fe2694575b886cbd9153227be'>Newsletter</a> | <a href='mailto:hello@recoding.tech'>hello@recoding.tech</a>"
+  footer.appendChild(footerInner);
+  console.log(footer);
+  document.body.appendChild(footer);
+});
