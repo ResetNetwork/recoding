9ed836b3defe10ae876fc66c2f7fb2c7b80b1b6b
Josh King <jking@chambana.net>
2021-02-12 14:20:40 -0500

    Eliminate the 'about' link from the footer menu.

--- src/super.js
+++ src/super.js
@@ -2,7 +2,7 @@ window.addEventListener('load', (event) => {
   const parent = document.getElementById('notion-app');
   const footer = document.createElement("FOOTER");
   const footerInner = document.createElement("DIV");
-  footerInner.innerHTML = "<a href='https://recoding.tech'>Home</a> | <a href='https://recoding.tech/About-7755d738441b41d3bf29dc2a679e0a9a'>About</a> | <a href='mailto:hello@recoding.tech'>hello@recoding.tech</a>"
+  footerInner.innerHTML = "<a href='https://recoding.tech'>Home</a> | <a href='mailto:hello@recoding.tech'>hello@recoding.tech</a>"
   footer.appendChild(footerInner);
   document.body.appendChild(footer);
 });
